f6c68f6f7254ade9a0787d522e71d92319ca84ec
Liwei Xia <liwei.xia0223@gmail.com>
2016-07-09 11:07:41 +0800

    base controller here for webrequest

---
+++ controller/apps_info.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/vendor/')
+#os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+import uuid
+
+hotpoor_apps = {
+    "hotpoor":{
+        "app_name_cn":u"Hotpoor序",
+        "app_name_en":u"Hotpoor",
+    }
+}
